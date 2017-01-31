a296684225243795d1203fd8710ff8414b9c15d0
atkallie <kal17da@gmail.com>
2017-01-31 13:32:49 -0500

    Initial Commit

--- isItPrime/ViewController.swift
+++ isItPrime/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var num: UITextField!
+    @IBOutlet var answer: UILabel!
+    
+    @IBAction func goButton(_ sender: Any) {
+        if let num2 = Int(num.text!){
+            if num2==2{
+                answer.text="Prime"
+            }else if num2==1{
+                answer.text="Neither"
+            }else if num2<=0{
+                answer.text="Enter a Number >0!"
+            }else{
+                for i in 2...(num2-1){
+                    if num2%i==0 {
+                        answer.text="Not Prime"
+                        break
+                    }else{
+                        answer.text="Prime"
+                    }
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
